0d18c4cbd888180761da7246bc63899d61e03920
arean82 <arean82@gmail.com>
2025-07-28 16:43:04 +0530

    update init

--- init.rb
+++ init.rb
@@ -4,7 +4,8 @@ Redmine::Plugin.register :redmine_percent_done do
   description 'Automatic issue done ratios for selected issue statuses, keeping the field editable manually.'
   author     'Jens Krämer'
   author_url 'https://jkraemer.net/'
-  version '2.0.0'
+  url 'https://github.com/Arean82/redmine_percent_done'
+  version '2.0.1'
   requires_redmine version_or_higher: '5.0.0'
   settings partial: 'settings/redmine_percent_done', default: {}
 end
